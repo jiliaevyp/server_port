b939a8b9d63b656c68a4a2449807c0c6e2463254
jiliaevyp <jiliaevyp@gmail.com>
2021-01-29 20:55:53 +0500

    Сервер для обслуживания запросов по сети tcp вводит тип сети "tcp" IP адрес и порт по умолчанию tcp 192.168.0.101:8181 запускается сообщает готовность отвечает на запрос

--- main.go
+++ main.go
@@ -3,8 +3,6 @@ package main
 import (
 	"errors"
 	"fmt"
-	"time"
-
 	//"io"
 	"net"
 	"os"
@@ -22,60 +20,139 @@ var ErrInvalidServerListen = errors.New("invalid listen server")
 const (
 	answerServer = "Hello, I am a server."
 	readyServer  = "I'm ready!"
+	defaultNet   = "tcp"
+	defaultIp    = "192.168.1.101"
+	defaultPort  = "8181"
 )
 
-// выбор протокола сети
-//Протокол должен представлять одно из значений: "tcp", "tcp4", "tcp6", "unix", "unixpacket".
+// заголовок
+func _beg() {
+	fmt.Println("-------------------------------------")
+	fmt.Println("|        	  'Go' сервер            |")
+	fmt.Println("|     Отвечаем незамедлительно!     |")
+	fmt.Println("|                                   |")
+	fmt.Println("|   (c) jiliaevyp@gmail.com         |")
+	fmt.Println("-------------------------------------")
+}
 
+// установка конфигурации сервера
+func _config() (string, string) {
+	var err int
+	var ip, port, net string
+	err = 1
+	for err == 1 {
+		net, err = inpNetwork() // ввод сети сервера
+		fmt.Println("Тип сети:   ", net, "\n")
+	}
+	err = 1
+	for err == 1 {
+		ip, err = inpIP() // ввод IP сервера
+		fmt.Println("IP адрес сервера:   ", ip, "\n")
+	}
+	err = 1
+	for err == 1 {
+		port, err = inpPort() // ввод порта сервера
+		fmt.Println("Порт сервера:   ", port, "\n")
+	}
+	return net, ip + port
+}
+
+// ввод типа протокола сети
 func inpNetwork() (string, int) {
 	var typNet string
+	var err int
 	len := 256
-	data := make([]byte, len)
-	n, err := os.Stdin.Read(data)
-	typNet = string(data[0 : n-1])
-	if err != nil || typNet != "tcp" {
-		return typNet, 1
-	} else {
-		return typNet, 0
-	}
-}
-
-func inpIP() {
-	err := 1
+	err = 1
 	for err == 1 {
-		fmt.Print("Введите IP сервера:	")
-		//inpIP()
-		fmt.Scanf(
-			"%s\n",
-			&_ip,
-		)
-		iperr := net.ParseIP(_ip)
-		if iperr != nil {
-			err = 0
+		fmt.Print("Тип сети по умолчанию:	", defaultNet, "\n", " Нажмите (Y) для изменения ")
+		yes := yesNo()
+		if yes == 0 {
+			typNet = defaultNet
+			return typNet, 0
 		} else {
-			fmt.Println(ErrInvalidIPaddress)
+			fmt.Print("Введите тип сети:	")
+			data := make([]byte, len)
+			n, err := os.Stdin.Read(data)
+			typNet = string(data[0 : n-1])
+			if err != nil || typNet != "tcp" {
+				fmt.Println(ErrInvalidTypeNetwork)
+				return typNet, 1
+			}
+			return typNet, 0
 		}
 	}
+	return typNet, 0
 }
 
-// ввод локального адреса
-//Локальный адрес может содержать только номер порта, например, ":8080".
-//В этом случае приложение будет обслуживать по всем.
+// ввод ip адреса сервера
+func inpIP() (string, int) {
+	data := ""
+	err := 1
+	for err == 1 {
+		fmt.Print("IP адрес сервера по умолчанию:	", defaultIp, "\n", "Для изменения нажмите 'Y' ")
+		yes := yesNo()
+		if yes != 1 {
+			data = defaultIp
+			err = 0
+		} else {
+			fmt.Print("Введите IP адрес сервера:	")
+			fmt.Scanf(
+				"%s\n",
+				&data,
+			)
+			iperr := net.ParseIP(data)
+			if iperr == nil {
+				fmt.Println(ErrInvalidIPaddress)
+				return data, 1
+			} else {
+				err = 0
+			}
+		}
+	}
+	return data, err
+}
+
+//ввод номера порта
 func inpPort() (string, int) {
 	var (
 		webPort string
-		res     float64
 	)
-	fmt.Scanf(
-		"%s\n",
-		&webPort,
-	)
-	res, err := strconv.ParseFloat(webPort, 16)
-	res = res + 1
-	if err != nil || len(webPort) != 4 {
-		return ":" + webPort, 1
+	err := 1
+	for err == 1 {
+		fmt.Print("Порт по умолчанию:	", defaultPort, "\n", "Для изменения нажмите 'Y' ")
+		yes := yesNo()
+		if yes != 1 {
+			webPort = defaultPort
+			err = 0
+		} else {
+			fmt.Print("Введите порт:	")
+			fmt.Scanf(
+				"%s\n",
+				&webPort,
+			)
+			res, err1 := strconv.ParseFloat(webPort, 16)
+			res = res + 1
+			err = 0
+			if err1 != nil {
+				fmt.Println(ErrInvalidPort)
+				return ":" + webPort, 1
+			}
+		}
+	}
+	return ":" + webPort, 0
+}
+
+// проверка на ввод  'Y = 1
+func yesNo() int {
+	var yesNo string
+	len := 4
+	data := make([]byte, len)
+	n, err := os.Stdin.Read(data)
+	yesNo = string(data[0 : n-1])
+	if err == nil && (yesNo == "Y" || yesNo == "y" || yesNo == "Н" || yesNo == "н") {
+		return 1
 	} else {
-		return ":" + webPort, 0
+		return 0
 	}
 }
 
@@ -106,7 +183,6 @@ func _server() {
 			return
 		}
 		go handleConnect(conn) // запуск обработчика запросов сервера
-		// нужен таймер отключения
 	}
 }
 
@@ -121,58 +197,31 @@ func handleConnect(conn net.Conn) {
 			break
 		}
 		requestMessage := string(input[0:n])
-		answerText := "На запрос " + requestMessage + "---> Ответ сервера: Все ништяк!"
+		fmt.Println("Получен запрос: " + requestMessage)
+		answerText := "Все ништяк!"
 		conn.Write([]byte(answerText)) //транслируем в порт ответ ввиде среза байт
+		fmt.Println("Отправлен ответ: ", answerText)
 	}
-}
-
-func _beg() {
-	fmt.Println("------------------------------------")
-	fmt.Println("|  Запуск Go server                |")
-	fmt.Println("|  Запускать, не перезапускать!    |")
-	fmt.Println("|                                  |")
-	fmt.Println("|   (c) jiliaevyp@gmail.com        |")
-	fmt.Println("------------------------------------")
 }
 
 func main() {
-	var komand string
-	err := 1
-	_beg() // заголовок
-	for err == 1 {
-		fmt.Print("Введите тип сети:	")
-		_network, err = inpNetwork()
-		if err == 1 {
-			fmt.Println(ErrInvalidTypeNetwork)
-		}
-	}
-	inpIP()
-	err = 1
-	for err == 1 {
-		fmt.Print("Введите номер порта:	")
-		_port, err = inpPort()
-		if err == 1 {
-			fmt.Println(ErrInvalidPort)
-		}
-	}
-	_port = _ip + _port
-	go _server() // запуск сервера
-	<-time.After(5 * time.Second)
-	if errServ == 0 {
-		komand = "Y"
-		for komand == "Y" || komand == "y" || komand == "Н" || komand == "н" {
-			fmt.Println("\n", "Закончить работу сервера? (Y)")
-			fmt.Scanf(
-				"%s\n",
-				&komand,
-			)
-			if komand == "Y" || komand == "y" || komand == "Н" || komand == "н" {
-				fmt.Println("\n", "Рад был с Вами пработать!")
-				fmt.Print("Обращайтесь в любое время без колебаний!", "\n", "\n")
-				return
-			}
-		}
-	} else {
+	var yes int
+	_beg()                      // заголовок
+	_network, _port = _config() // конфигурация сервера
+	go _server()                // запуск сервера
+	//<-time.After(5 * time.Second)
+	if errServ != 0 { // ошибка сервера
 		fmt.Println(ErrInvalidServerListen)
+		fmt.Println("\n", "Сервер остановлен")
+		return
+	} else {
+		fmt.Println("\n", "Закончить работу сервера? (Y)")
+		yes = yesNo()
+		if yes == 1 {
+			fmt.Println("\n", "Сервер остановлен")
+			fmt.Println("\n", "Рад был на Вас пработать!")
+			fmt.Print("Обращайтесь в любое время без колебаний!", "\n", "\n")
+			return
+		}
 	}
 }
